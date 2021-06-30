a989ee62ef26d9dfc2020abe8ef1cbc9d0b64cef
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-06-30 13:28:27 +0300

    Project Euler - solution of problem 7 added.

--- Week-02/Homeworks.playground/Contents.swift
+++ Week-02/Homeworks.playground/Contents.swift
@@ -25,3 +25,51 @@ let result = Int(pow(Double(sum),Double(2))) - sumOfTheSquares
 
 print("Difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is \(result)")
 
+/**
+ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+ What is the 10 001st prime number?
+ */
+
+var current: Int = 2
+var primeNumbers: [Int: Int] = [:]
+var minusOne: Int = 0
+var plusOne: Int = 0
+var isPrime: Bool
+
+for i in 1... {
+    // 6n - 1 and 6n + 1 -> 2 and 3 are the exceptions therefore counts starts with 2.
+    minusOne = 6 * i - 1
+    plusOne = 6 * i + 1
+    
+    isPrime = true
+    for k in 2..<Int(sqrt(Double(minusOne))) + 1 {
+        if(minusOne % k == 0) {
+            isPrime = false
+            break
+        }
+    }
+    if(isPrime) {
+        current += 1
+        primeNumbers[current] = minusOne
+    }
+    
+    isPrime = true
+    for k in 2..<Int(sqrt(Double(plusOne))) + 1 {
+        if(plusOne % k == 0) {
+            isPrime = false
+            break
+        }
+    }
+    if(isPrime) {
+        current += 1
+        primeNumbers[current] = plusOne
+    }
+    
+    
+    if(current >= 10001) {
+        break
+    }
+}
+
+print("10 001st prime number: \(primeNumbers[10001] as Any)")
